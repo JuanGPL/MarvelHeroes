64e00176c48a6d450b20027051032528055fbd79
Juan Leo <juanleodev@gmail.com>
2021-06-16 20:48:38 +0200

    Splash added

--- app/src/main/java/com/juanleodev/marvelheroes/MainActivity.kt
+++ app/src/main/java/com/juanleodev/marvelheroes/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.juanleodev.marvelheroes
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.juanleodev.marvelheroes.databinding.ActivityMainBinding
+import com.juanleodev.marvelheroes.heroeslist.HeroesListActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        animationWatcher()
+
+    }
+
+    private fun animationWatcher() {
+        var animationFinihed = false
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                while(!animationFinihed) {
+                    if (binding.splashMotionContainer.progress == 1.0f) {
+                        animationFinihed = true
+                        delay(TimeUnit.SECONDS.toMillis(1))
+                        val intent = Intent(this@MainActivity, HeroesListActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/juanleodev/marvelheroes/heroeslist/HeroesListActivity.kt
@@ -0,0 +1,14 @@
+package com.juanleodev.marvelheroes.heroeslist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.juanleodev.marvelheroes.R
+
+class HeroesListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_heroes_list)
+
+    }
+}
