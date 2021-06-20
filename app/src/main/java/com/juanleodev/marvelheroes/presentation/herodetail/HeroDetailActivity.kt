9883655f479b3e5f3622f2885ab731ccbceaaa78
Juan Leo <juanleodev@gmail.com>
2021-06-20 14:24:01 +0200

    Default text in detail when there is no description

--- app/src/main/java/com/juanleodev/marvelheroes/presentation/herodetail/HeroDetailActivity.kt
+++ app/src/main/java/com/juanleodev/marvelheroes/presentation/herodetail/HeroDetailActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.juanleodev.marvelheroes.BuildConfig
+import com.juanleodev.marvelheroes.R
 import com.juanleodev.marvelheroes.databinding.ActivityHeroDetailBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -39,8 +40,16 @@ class HeroDetailActivity : AppCompatActivity() {
                     .into(imgHeroDetail)
 
                 tvHeroNameDetail.text = it.name
-                tvHeroDescription.text = it.description
+                tvHeroDescription.text = checkDescriptionContent(it.description)
             }
         })
+    }
+
+    private fun checkDescriptionContent(content: String?): String {
+        if (content.isNullOrEmpty()) {
+            return getString(R.string.no_description)
+        }
+
+        return content
     }
 }
